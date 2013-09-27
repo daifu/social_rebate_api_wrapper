69cd7ad04435ec90ed0ee9cdfce94cbe2245afde
daifu <daifu.ye@gmail.com>
2013-09-26 17:00:46 -0700

    fixed the tests

--- spec/social_rebate_spec.rb
+++ spec/social_rebate_spec.rb
@@ -6,7 +6,7 @@ describe SocialRebate do
     @creds[:api_key]    = SocialRebate::Config.api_key
     @creds[:api_secret] = SocialRebate::Config.api_secret
     @creds[:store_key]  = SocialRebate::Config.store_key
-    @cn = SocialRebate::Connection.new(@creds)
+    @cn = {}
     SocialRebate::Connection.stub(:new).with(@creds).and_return(@cn)
     @token  = 'token'
     @option = {:order_email => 'email', :total_purchase => '10', :order_id => 1000}
@@ -15,7 +15,7 @@ describe SocialRebate do
   describe "verify" do
     it "should receive correct params for put request" do
       @option[:status] = "VERIFIED"
-      @cn.stub(:put).with("/api/v2/orders/#{@token}/", @option)
+      @cn.should_receive(:put).with("/api/v2/orders/#{@token}/", @option)
       SocialRebate.verify(@token, @option)
     end
   end
@@ -23,14 +23,14 @@ describe SocialRebate do
   describe "cancel" do
     it "should receive correct params for cancel request" do
       @option[:status] = "VOID"
-      @cn.stub(:put).with("/api/v2/orders/#{@token}/", @option)
-      SocialRebate.verify(@token, @option)
+      @cn.should_receive(:put).with("/api/v2/orders/#{@token}/", @option)
+      SocialRebate.cancel(@token, @option)
     end
   end
 
   describe "init" do
     it "should receive correct params for social rebate request" do
-      @cn.stub(:post).with("/api/v2/orders/", @option)
+      @cn.should_receive(:post).with("/api/v2/orders/", @option)
       SocialRebate.init(@option)
     end
   end
